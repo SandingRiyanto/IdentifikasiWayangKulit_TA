0a8785b60d05dccadb001a2456245ff48d6ff1c1
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-06-13 23:25:59 +0700

    13 juni 2022

--- testvideodetect.py
+++ testvideodetect.py
@@ -2,37 +2,33 @@
 from cv2 import cv2
 import numpy as np
 from keras.models import model_from_json
+import tensorflow as tf
+import tensorflow_hub as hub
+from keras.models import load_model
 
-# create dict from label images
-wayang_dict = {0:"abimanyu", 1:"anoman", 2:"arjuna", 3:"bagong", 4:"baladewa", 5:"bathara_guru", 6:"bathara_indra", 7:"bathara_kala",
-            8:"bathara_narada", 9:"bima", 10:"buta", 11:"cakil", 12:"durna", 13:"dursasana", 14:"duryudana", 15:"gareng", 16:"gatotkaca",
-            17:"karna", 18:"kresna", 19:"nakula_sadewa", 20:"patih_sabrang", 21:"petruk", 22:"puntadewa", 23:"semar", 24:"sengkuni",
-            25:"togog"
-}
-
-# load json and create model
-json_file = open('model/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-wayang_model = model_from_json(loaded_model_json)
-
-# load weight into model
-wayang_model.load_weights("model/model.h5")
+# load model .h5
+model_path = 'model/wayang_model_new_1.h5'
+wayang_model = tf.keras.models.load_model((model_path),custom_objects={'KerasLayer':hub.KerasLayer})
 print("loaded model from disk")
 
+# define variabel array untuk kelas wayang
+wayang_class = ["abimanyu", "anoman", "arjuna", "bagong", "baladewa", "bima", "buta", "cakil", "durna", "dursasana", "duryudana",
+                "gareng", "gatotkaca", "karna", "kresna", "nakula_sadewa", "patih_sabrang", "petruk", "puntadewa", "semar", "sengkuni", "togog"
+]
+
 # start load video test
-cap = cv2.VideoCapture("directory video...")
+cap = cv2.VideoCapture("testing_vid\video3.mp4")
 while True:
     # find haar-cascade to draw bounding box arround face -> wayang
     ret, frame = cap.read()
     frame = cv2.resize(frame, (1280, 720))
     if not ret:
         break
-    face_detector = cv2.CascadeClassifier('opencv_xml\haarcascade_eye.xml') #ganti dg deteksi objek wayang (mgkin)
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # face_detector = cv2.CascadeClassifier('opencv_xml\haarcascade_eye.xml') #ganti dg deteksi objek wayang (mgkin)
+    # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # detect face available on cam
-    num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+    # # detect face available on cam
+    # num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
     # take each face available on cam and process
     for(x,y,w,h) in num_faces:
