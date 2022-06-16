c6f5101040649e8f233dd94f5a0bdcf74afb3f3d
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-06-16 20:21:18 +0700

    done

--- identifyimg.py
+++ identifyimg.py
@@ -50,10 +50,10 @@ def load_image(img_path, show=False):
     cv2.imshow("Gambar", img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.Canny(img, 100, 200)
-    cv2.imwrite("citra_uji.jpg", img)
+    cv2.imwrite("simpan/citra_uji.jpg", img)
 
     # ubah citra ke array dh expand_dims
-    img = image.load_img("D:/Coding/My_Github/VidClass_DeepLearn/citra_uji.jpg", target_size=(224, 224))
+    img = image.load_img("D:/Coding/My_Github/VidClass_DeepLearn/simpan/citra_uji.jpg", target_size=(224, 224))
     img_tensor = image.img_to_array(img)                  # (height, width, channels)
     img_tensor = np.expand_dims(img_tensor, axis=0)       # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
     img_tensor /= 255.                                    # imshow expects values in the range [0, 1]
