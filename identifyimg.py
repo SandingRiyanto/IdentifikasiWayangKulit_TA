202d7a11e110e3d15154fb10fc0aa2f4cf19e73d
SandingRiyanto <riyanto.sanding.13@gmail.com>
2022-06-16 10:31:38 +0700

    16 juni 2022, done

--- identifyimg.py
+++ identifyimg.py
@@ -31,7 +31,7 @@ opsi = {'padx':5, 'pady':5}
 # load model .h5
 model_path = 'model/wayang_model_new_1.h5'
 wayang_model = tf.keras.models.load_model((model_path),custom_objects={'KerasLayer':hub.KerasLayer})
-print("loaded model from disk")
+# print("loaded model from disk")
 
 # define variabel array untuk kelas wayang
 wayang_class = ["abimanyu", "anoman", "arjuna", "bagong", "baladewa", "bima", "buta", "cakil", "durna", "dursasana", "duryudana",
@@ -40,25 +40,31 @@ wayang_class = ["abimanyu", "anoman", "arjuna", "bagong", "baladewa", "bima", "b
 
 # menampilkan label
 tk.Label(root,
-        text="Hasil Identifikasi dari tokoh wayang:",
+        text="Hasil Identifikasi:",
         font="Helvetica 14 bold italic",
         fg="black").pack()
 
 # fungsi: me-load image testing dan mengubahnya ke dalam array dims
 def load_image(img_path, show=False):
     img = cv2.imread(img_path)
-    cv2.imshow("Gambar", img)
 
     # coba ya
 
     # img = image.load_img(img_path, target_size=(224, 224))
     # img.show()
-
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (224,224))
+    cv2.imshow("Gambar", img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # # img = cv2.cvtColor(np.float32(img), cv2.COLOR_RGB2GRAY)
-    # img = cv2.Canny(img, 100, 200)
+    img = cv2.Canny(img, 100, 200)
+    
+    cv2.imwrite("simpan.jpg", img)
     # print(type(img))
 
+    currdir = os.getcwd()
+    img_path2= filedialog.askopenfilename(initialdir=currdir, title="Choose an image", filetypes=(("all files", "*.*"), ("png files", "*.png")))
+    
+    img = image.load_img(img_path2, target_size=(224, 224))
     img_tensor = image.img_to_array(img)                    # (height, width, channels)
     img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
     img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
@@ -100,14 +106,12 @@ if pred is not None:
     # print("{}".format(wayang_class[top[0]])+" ({:.3})".format(pred[0][top[0]]))
     # plt.imshow(new_image)
 
+    hasil = "{}".format(wayang_class[top[0]])+" ({:.2})".format(pred[0][top[0]])
+    # print(hasil)
     nama_wayang = "{}".format(wayang_class[top[0]])
-
-    hasil = "{}".format(wayang_class[top[0]])+" ({:.3})".format(pred[0][top[0]])
-    print(hasil)
-
     # cek apakah nama folder sesuai dengan hasil prediksi
     if nama_folder == nama_wayang:
-        print("ya, betul")
+        # print("ya, betul")
 
         # show hasil
         tk.Label(root, 
@@ -126,12 +130,6 @@ if pred is not None:
             font = "Verdana 12 bold").pack()
         messagebox.showinfo("Show Info", "SALAH! Data Uji salah diidentifikasi")
         button2 = ttk.Button(root, text='Identify Again', command=root.destroy, width=80).pack(pady=10)
-        
-# button2 = ttk.Button(root, text='Identify Again', command=root.destroy, width=100).pack()
-# i=0
-# for i in range(22):
-#     cm = confusion_matrix(wayang_class[i], pred)
-#     print(cm)
-#     i +=1
+
 # start the app
 root.mainloop()
